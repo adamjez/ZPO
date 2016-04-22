dbc89845c9e1c78ed1a2a07d76ad29cb64b641e0
Adam Jež <adamjez@outlook.cz>
2016-04-22 21:09:52 +0200

    Reworked gaussian probability model

--- ZPO.Core/Conditions/GaussianColorsCondition.cs
+++ ZPO.Core/Conditions/GaussianColorsCondition.cs
@@ -1,6 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Automation;
+using MathNet.Numerics.LinearAlgebra;
 using ZPO.Core.Colors;
 
 namespace ZPO.Core.Conditions
@@ -8,57 +13,92 @@ namespace ZPO.Core.Conditions
     public class GaussianColorsCondition : ColorsCondition
     {
         private double threshold = 0.005;
-        protected Point3D Mean;
-        protected Point3D Deviation;
+        protected Matrix<double> CovarianceMatrix;
+        protected Vector<double> MeanVector;
         public GaussianColorsCondition(List<IColor> compareColors, uint tolerance, uint toleranceMultiplier = 1)
             : base(compareColors, tolerance, toleranceMultiplier)
         {
-            // Create gaussian model
-            Mean = new Point3D
-            {
-                X = compareColors.Select(color => color.GetFirstPart()).Average(),
-                Y = compareColors.Select(color => color.GetSecondPart()).Average(),
-                Z = compareColors.Select(color => color.GetThirdPart()).Average()
-            };
+            MeanVector = CreateVector.DenseOfArray(
+                new double[]
+                {
+                    compareColors.Select(color => color.GetFirstPart()).Average(),
+                    compareColors.Select(color => color.GetSecondPart()).Average(),
+                    compareColors.Select(color => color.GetThirdPart()).Average()
+                });
 
-            Deviation = new Point3D()
+            var firstPartList = compareColors.Select(color => color.GetFirstPart()).ToList();
+            var secondPartList = compareColors.Select(color => color.GetSecondPart()).ToList();
+            var thirdPartList = compareColors.Select(color => color.GetThirdPart()).ToList();
+
+            CovarianceMatrix = CreateMatrix.DenseOfArray(new double[,]
             {
-                X = ComputeDeviation(compareColors.Select(color => color.GetFirstPart()), Mean.X),
-                Y = ComputeDeviation(compareColors.Select(color => color.GetSecondPart()), Mean.Y),
-                Z = ComputeDeviation(compareColors.Select(color => color.GetThirdPart()), Mean.Z)
-            };
+                {
+                    ComputeDeviation(firstPartList, MeanVector[0], firstPartList, MeanVector[0]),
+                    ComputeDeviation(firstPartList, MeanVector[0], secondPartList, MeanVector[1]),
+                    ComputeDeviation(firstPartList, MeanVector[0], thirdPartList, MeanVector[2])
+                },
+                {
+                    ComputeDeviation(secondPartList, MeanVector[1], firstPartList, MeanVector[0]),
+                    ComputeDeviation(secondPartList, MeanVector[1], secondPartList, MeanVector[1]),
+                    ComputeDeviation(secondPartList, MeanVector[1], thirdPartList, MeanVector[2])
+                },
+                {
+                    ComputeDeviation(thirdPartList, MeanVector[2], firstPartList, MeanVector[0]),
+                    ComputeDeviation(thirdPartList, MeanVector[2], secondPartList, MeanVector[1]),
+                    ComputeDeviation(thirdPartList, MeanVector[2], thirdPartList, MeanVector[2])
+                }
+            });
         }
+
+        private double min = 1;
+        private double max = 0;
 
         public override bool Compare(IColor pixelColor, int neighborCount)
         {
-            var result = GaussianFunction(pixelColor.GetFirstPart(), Mean.X, Deviation.X);
-            result += GaussianFunction(pixelColor.GetSecondPart(), Mean.Y, Deviation.Y);
-            result += GaussianFunction(pixelColor.GetThirdPart(), Mean.Z, Deviation.Z);
+            var result = GaussianFunction3D(
+                CreateVector.DenseOfArray(
+                    new double[]
+                    {
+                        pixelColor.GetFirstPart(),
+                        pixelColor.GetSecondPart(),
+                        pixelColor.GetThirdPart()
+                    }));
 
+            if (result > max)
+            {
+                Debug.WriteLine($"new max is {result}");
+            }
+            if (result < min)
+            {
+                Debug.WriteLine($"new min is {result}");
+            }
+            max = Math.Max(result, max);
+            min = Math.Min(result, min);
 
             return result > threshold;
         }
 
-        private double GaussianFunction(int value, double mean, double deviation)
+        private double GaussianFunction3D(Vector<double> value)
         {
-            var a = 1 / (deviation * Math.Sqrt(2 * Math.PI));
-            var b = -Math.Pow(value - mean, 2) / (2 * Math.Pow(deviation, 2));
+            var a = 1 / Math.Sqrt(CovarianceMatrix.Determinant() * Math.Pow(2 * Math.PI, 3));
+            var b = -1.0 / 2.0 * (value - MeanVector).ToRowMatrix() * CovarianceMatrix.Inverse() * (value - MeanVector);
 
-            return a * Math.Exp(b);
+            return a * Math.Exp(b[0]);
         }
 
-        private double ComputeDeviation(IEnumerable<int> values, double mean)
+        private double ComputeDeviation(List<int> values1, double mean1, List<int> values2, double mean2)
         {
-            double accumulator = values.Sum(value => Math.Pow(value - mean, 2));
+            if (values1.Count() != values2.Count())
+                throw new ArgumentException("Arguments have to have same dimensions");
 
-            return Math.Sqrt(accumulator); ;
+            double accumulator = 0;
+            var size = values1.Count;
+            for (int i = 0; i < size; i++)
+            {
+                accumulator += (values1[i] - mean1) * (values2[i] - mean2);
+            }
+
+            return accumulator / size;
         }
-    }
-
-    public struct Point3D
-    {
-        public double X;
-        public double Y;
-        public double Z;
     }
 }
